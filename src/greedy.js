7e89c633724dd4f7fa2e968c85ae46d2405c2504
mymungit <37037436+mymungit@users.noreply.github.com>
2018-04-12 11:01:56 +0700

    Add files via upload

---
+++ src/greedy.js
@@ -0,0 +1,31 @@
+
+
+
+
+function solve(){
+	initData();
+	var jalur = [0];
+	
+	for(var i=0;i<points.length;i++){
+		var min = 1e10;
+		var next=null;
+		for(var j=0;j<points.length;j++){
+			if(i!=j && jalur.includes(j)==false){
+				if (distance(points[i],points[j])<min){
+					min = distance(points[i],points[j]);
+					next = j;
+				}
+			}
+		}
+		if (next!=null){
+			jalur.push(next);
+		}
+	}
+	jalur.push(0);
+	alldistance = 0;
+	for(var i=0;i<jalur.length-1;i++){
+		alldistance=alldistance+distance(points[jalur[i]],points[jalur[i+1]]);
+	}
+	drawLines(jalur);
+	alert("greedy distance: " +Math.round(alldistance));
+}
