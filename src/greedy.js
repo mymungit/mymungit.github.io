5b2c48007719b3f97a918b8e38ef98c58f458993
mymungit <37037436+mymungit@users.noreply.github.com>
2018-04-15 09:35:43 +0700

    Update greedy.js

--- src/greedy.js
+++ src/greedy.js
@@ -1,11 +1,7 @@
 
-function solve(){
-	clearCanvas();
-	
-	for(var i=0; i<points.length; i++) {
-      		drawCircle(cities[i],points[i]);
-	}
-	var jalur = [0];
+
+function greedy(awal){
+	var jalur = [awal];
 	
 	for(var i=0;i<points.length;i++){
 		var min = 1e10;
@@ -22,11 +18,40 @@ function solve(){
 			jalur.push(next);
 		}
 	}
-	jalur.push(0);
-	alldistance = 0;
+	jalur.push(awal);
+	jarak = 0;
 	for(var i=0;i<jalur.length-1;i++){
-		alldistance=alldistance+distance(points[jalur[i]],points[jalur[i+1]]);
+		jarak=jarak+distance(points[jalur[i]],points[jalur[i+1]]);
 	}
+	
+	return [jarak,jalur];
+}
+
+
+function solve(){
+	clearCanvas();
+	
+	for(var i=0; i<points.length; i++) {
+      drawCircle(cities[i],points[i]);
+    }
+	
+	var alldistance = 1e10;
+	var jalur = [];
+	
+	
+	for(var i=0;i<points.length;i++){
+		hasil = greedy(i);
+		dis = hasil[0];
+		jal = hasil[1];
+		console.log(dis);
+		if(dis<alldistance){
+			alldistance = dis;
+			jalur = jal;
+		}
+	}
+	console.log(jalur);
 	drawLines(jalur);
-	alert("greedy distance: " +Math.round(alldistance));
+	t1g = performance.now();
+	running = false;
+	alert("greedy distance: " +Math.round(alldistance)+ " calculation time: "+ Math.round((t1g-t0g))+" milliseconds");
 }
